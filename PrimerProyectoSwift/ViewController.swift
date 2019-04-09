4a9c04fdb2cb1f32a3bb03304ede268d0e2752fe
CharlieSantin <josecarloscruzsantin@outlook.com>
2019-04-09 16:44:51 -0500

    Initial Commit

---
+++ PrimerProyectoSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimerProyectoSwift
+//
+//  Created by José Carlos Cruz Santin  on 4/9/19.
+//  Copyright © 2019 Charlies Santin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PrimerProyectoSwiftTests/PrimerProyectoSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  PrimerProyectoSwiftTests.swift
+//  PrimerProyectoSwiftTests
+//
+//  Created by José Carlos Cruz Santin  on 4/9/19.
+//  Copyright © 2019 Charlies Santin. All rights reserved.
+//
+
+import XCTest
+@testable import PrimerProyectoSwift
+
+class PrimerProyectoSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
